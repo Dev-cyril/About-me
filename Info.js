cf100212278d81f4fe7c06d3fc54ee907e1e08d1
Cyril <cyril.idu2@gmail.com>
2022-06-21 15:25:56 +0100

    About me

---
+++ Info.js
@@ -0,0 +1,6 @@
+const name = prompt("enter your name: ");
+const height = prompt("Enter your height: ");
+const country = prompt("Enter your Country: ");
+
+alert("Name: " +  name + "\n" + "Height: " + height + "\n" + "Country: " + country);
+console.log("Name: " +  name + "\n" + "Height: " + height + "\n" + "Country: " + country);
